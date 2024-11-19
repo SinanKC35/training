f9e547059f6155345ef8e0ee9fa8759cfd9ab4d1
SinanKC35 <149432470+SinanKC35@users.noreply.github.com>
2024-11-19 15:40:54 +0200

    Update test.c

--- test.c
+++ test.c
@@ -2,7 +2,7 @@
 
 int main() {
     printf("Hello, World!\n");
-    printf("fuck you dude/n");
-    printf("fuck you dude/n");
+    printf("fuck you dude\n");
+    printf("fuck you dude\n");
     return 0;
 }
